1db74c1c191ed72547cfd9a09e62a9408ea1754c
Carlos Rodrigo SMV <luxor90@gmail.com>
2017-06-05 22:31:49 +0200

    Modificado AdaptadorDAO y su Test
    Agregado un método para obtener una lista con todos los objetos, sea un
    tipo lista o no, aunque en privado para no tener que testearlo por si no
    lo acabamos necesitando
    
    By Xaldre

--- src/control/adaptador/AdaptadorDAO.java
+++ src/control/adaptador/AdaptadorDAO.java
@@ -1,11 +1,14 @@
 package control.adaptador;
 
+import java.util.ArrayList;
+
 import modelo.DAO;
 import utiles.Tipo;
 
 public class AdaptadorDAO {
 	protected Tipo tipo;
 	protected DAO dao;
+	protected boolean started = false;
 
 	// obligo al que crea el objeto a decirme que tipo es
 	public AdaptadorDAO(Tipo tipo) {
@@ -21,24 +24,56 @@ public class AdaptadorDAO {
 		dao = new DAO(tipo.getRuta(), tipo.isLista());
 		return dao.grabar(obj);
 	}
+	
+	public boolean add(Object obj){
+		if (tipo.isLista()){
+			ArrayList list = new ArrayList<>();
+			list = (ArrayList) getNext();
+			list.add(obj);
+			return escribir(list);
+		} else {
+			return escribir(obj);
+		}
+	}
 
 	public Object getNext() {
-		dao = new DAO(tipo.getRuta(), tipo.isLista());
+		if (tipo.isLista() || !started) {
+			dao = new DAO(tipo.getRuta(), tipo.isLista());
+		}
 		return dao.leer();
 	}
-	
-	public int buscar(Object obj){
+
+	/**
+	 * Retorna una lista con todos los elementos que se encuentren en el archivo
+	 * especificado por el tipo
+	 * 
+	 * @return ArrayList con elementos
+	 */
+		if (tipo.isLista()) {
+			return getNext();
+		} else {
+			ArrayList list = new ArrayList<>();
+			Object object = getNext();
+			while (object != null) {
+				list.add(object);
+				object = getNext();
+			}
+			return list;
+		}
+	}
+
+	public int buscar(Object obj) {
 		dao = new DAO(tipo.getRuta(), tipo.isLista());
 		return dao.buscar(obj);
 	}
-	
-	public boolean exist(Object obj){
-		dao = new DAO(tipo.getRuta(),tipo.isLista());
-		return dao.exist(obj);		
+
+	public boolean exist(Object obj) {
+		dao = new DAO(tipo.getRuta(), tipo.isLista());
+		return dao.exist(obj);
 	}
-	
-	public Object obtener(Object obj){
-		dao = new DAO(tipo.getRuta(),tipo.isLista());
+
+	public Object obtener(Object obj) {
+		dao = new DAO(tipo.getRuta(), tipo.isLista());
 		return dao.obtener(obj);
 	}
 

f9953f66ffeebc0d28b221ac22e108e0056ff49a
Carlos Rodrigo <luxor90@gmail.com>
2017-06-06 10:40:38 +0200

    Test GUI - altaArticulo
    Test inicial del primer pane
    By Xaldre

---
+++ src/test/vista/articulo/testAltaClientePane.java
@@ -0,0 +1,44 @@
+package test.vista.articulo;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import vista.cliente.altaClientePane;
+
+public class testAltaClientePane extends JFrame {
+
+//	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					testAltaClientePane frame = new testAltaClientePane();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public testAltaClientePane() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+//		contentPane = new JPanel();
+//		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+//		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(new altaClientePane());
+	}
+
+}
